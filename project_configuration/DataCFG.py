c49eaa9db9e86549f4e3584f22f7e40bc4558720
MaxUhl98 <frozen2ice@web.de>
2024-07-18 05:00:47 +0200

    commit new configuration files

---
+++ project_configuration/DataCFG.py
@@ -0,0 +1,3 @@
+class DataCFG:
+    """Class that holds information about the projects data"""
+    time_column: str = 'date'
---
+++ project_configuration/TrainCFG.py
@@ -0,0 +1,3 @@
+class TrainCFG:
+    """Class to configure the training process"""
+    validation_time_steps: int = 60
