343c28c9e88cb244f4ccb6d3c644fb82c8d1bc73
MaxUhl98 <frozen2ice@web.de>
2024-07-19 18:58:04 +0200

    polished Train CFG class by adding the fiunction to save and load configurations, also wrote unittests with test cases

--- project_configuration/TrainCFG.py
+++ project_configuration/TrainCFG.py
@@ -1,12 +1,35 @@
+from ast import literal_eval
+
+
 class TrainCFG:
     """Class to configure the training process"""
     run_name: str = 'test'
-
     validation_time_steps: int = 60
     num_folds: int = 10
-
     run_save_directory: str = 'runs/'
 
+    def __init__(self):
+        """This is necessary for the saving of variables
+        (__dict__ values do not get set for class attributes)"""
+        self.run_name = self.run_name
+        self.validation_time_steps = self.validation_time_steps
+        self.num_folds = self.num_folds
+        self.run_save_directory = self.run_save_directory
+
     def save(self, save_path: str) -> None:
+        """Saves the current configuration setup to a txt file at a given location
+
+        :param save_path: Destination path to save the configuration setup to
+        :return: None
+        """
         with open(save_path, 'w') as f:
             f.write(str({k: v for k, v in vars(self).items() if not callable(v) and not k.startswith("__")}))
+
+    def load(self, load_path: str) -> None:
+        """Loads the configuration setup from a txt file at a given location
+
+        :param load_path: Path to the configuration setup txt file
+        :return: None
+        """
+        with open(load_path, 'r') as f:
+            self.__dict__.update(literal_eval(f.read()))
---
+++ unittests/test_project_configuration/test_TrainCFG.py
@@ -0,0 +1,36 @@
+import pytest
+import unittest
+import os
+from project_configuration.TrainCFG import TrainCFG
+from ast import literal_eval
+
+
+class TestTrainCFG(unittest.TestCase):
+
+    def setUp(self):
+        self.cfg = TrainCFG()
+        print(self.cfg.__dict__)
+        self.save_directory = 'files'
+        self.value_dict = {
+            'run_name': 'test',
+            'validation_time_steps': 60,
+            'num_folds': 10,
+            'run_save_directory': 'runs/'}
+
+    def tearDown(self):
+        del self.cfg
+        del self.save_directory
+        del self.value_dict
+
+    def test_save(self):
+        self.setUp()
+        self.cfg.save(self.save_directory + '/test.txt')
+        assert os.path.isfile(self.save_directory + '/test.txt')
+        with open(self.save_directory + '/test.txt', 'r') as f:
+            assert literal_eval(f.read()) == self.value_dict
+
+    def test_load(self):
+        self.setUp()
+        self.cfg.load(self.save_directory + '/load_test.txt')
+        assert {k: v for k, v in vars(self.cfg).items() if not callable(v) and not k.startswith("__")} == {
+            'run_name': 'test_new', 'validation_time_steps': 10, 'num_folds': 100, 'run_save_directory': 'runs/test/'}
