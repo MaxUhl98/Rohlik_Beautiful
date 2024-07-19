86f7cbe0470a334fb35f9d7cef8c3da56384282b
MaxUhl98 <frozen2ice@web.de>
2024-07-19 05:02:57 +0200

    added ability to save state of train cfg (need to load a state next)

--- project_configuration/TrainCFG.py
+++ project_configuration/TrainCFG.py
@@ -1,3 +1,12 @@
 class TrainCFG:
     """Class to configure the training process"""
+    run_name: str = 'test'
+
     validation_time_steps: int = 60
+    num_folds: int = 10
+
+    run_save_directory: str = 'runs/'
+
+    def save(self, save_path: str) -> None:
+        with open(save_path, 'w') as f:
+            f.write(str({k: v for k, v in vars(self).items() if not callable(v) and not k.startswith("__")}))
