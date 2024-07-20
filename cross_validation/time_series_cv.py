f462c7aef2e9626e303f54f33ade2c8c747558a8
MaxUhl98 <frozen2ice@web.de>
2024-07-20 05:20:58 +0200

    commit simplified cv loop with comments

--- cross_validation/time_series_cv.py
+++ cross_validation/time_series_cv.py
@@ -1,45 +1,136 @@
-from sklearn.model_selection import TimeSeriesSplit
+import logging
+import os
+from datetime import datetime
+from typing import Any, Callable, Iterable, Tuple
+import numpy as np
 import pandas as pd
+from sklearn.model_selection import TimeSeriesSplit
 from project_configuration.DataCFG import DataCFG
 from project_configuration.TrainCFG import TrainCFG
-import numpy as np
-from datetime import datetime
-from typing import *
-import os
+from utils.log_helpers import get_logger
 
 
 def get_sorted_timeseries_array(series: pd.Series) -> np.ndarray:
-    series = series.unique().tolist()
-    series.sort(key=lambda x: datetime.strptime(x, '%Y-%m-%d'))
-    return np.array(series)
+    """Convert series to a sorted numpy array of unique dates.
+
+    :param series: Series to create the sorted array from.
+    :return: Sorted numpy array of unique dates.
+    """
+    return np.array(sorted(series.unique(), key=lambda x: datetime.strptime(x, '%Y-%m-%d')))
 
 
 def create_new_save_directory(train_cfg: TrainCFG) -> str:
-    n = 1
-    save_dir_path = train_cfg.run_save_directory + train_cfg.run_name
-    while 1:
+    """Create a new save directory, incrementing suffix if it already exists.
 
-        if not os.path.exists(save_dir_path):
-            os.mkdir(save_dir_path)
-            break
-        else:
-            n += 1
-            save_dir_path = f'{train_cfg.run_save_directory + train_cfg.run_name}_{n}'
+    :param train_cfg: TrainCFG object used to determine the directory name
+    :return: Path of the new save directory
+    """
+    base_dir = os.path.join(train_cfg.run_save_directory, train_cfg.run_name)
+    save_dir_path = base_dir
+    n = 1
+    while os.path.exists(save_dir_path):
+        save_dir_path = f'{base_dir}_{n}'
+        n += 1
+    os.mkdir(save_dir_path)
     return save_dir_path
 
 
-def execute_cv_loop(_data: pd.DataFrame, train_function: Callable, train_cfg: TrainCFG, data_cfg: DataCFG,
-                    **train_kwargs) -> None:
+def get_fold_data(
+    _data: pd.DataFrame, data_cfg: DataCFG, time_series: np.ndarray,
+    train_times: np.ndarray, test_times: np.ndarray
+) -> Tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
+    """Split the data into training and testing sets based on time series.
+
+    :param _data: Pandas dataframe containing train and test data
+    :param data_cfg: Configuration for data usage
+    :param time_series: Array containing all unique sorted time steps of data
+    :param train_times: Indices of train dates in time series
+    :param test_times: Indices of test dates in time series
+    :return: Tuple containing (X_train, y_train, X_test, y_test)
+    """
+    X_train = _data[_data[data_cfg.time_column] <= time_series[train_times[-1]]]
+    X_test = _data[
+        (_data[data_cfg.time_column] >= time_series[test_times[0]]) &
+        (_data[data_cfg.time_column] <= time_series[test_times[-1]])
+    ]
+    y_train, y_test = X_train.pop(data_cfg.target_column), X_test.pop(data_cfg.target_column)
+    return X_train.values, y_train.values, X_test.values, y_test.values
+
+
+def log_and_save_results(
+    train_data: dict[str, Any], additional_metrics: Iterable[Callable],
+    additional_metric_losses: dict[str, Any], oof_predictions: list[Any],
+    cv_train_losses: list[Any], cv_test_losses: list[Any], cv_logger: logging.Logger,
+    y_test: np.ndarray, num: int
+) -> None:
+    """Log and save cross-validation results.
+
+    :param train_data: Data returned by the train function.
+    :param additional_metrics: List of metrics to use outside the train loop
+    :param additional_metric_losses: Dictionary containing lists of additional metric values from previous folds
+    :param oof_predictions: List of oof predictions from previous folds
+    :param cv_train_losses: List of train losses from previous folds
+    :param cv_test_losses: List of test losses from previous folds
+    :param cv_logger: Logger to use for cross-validation results
+    :param y_test: Test targets from current fold
+    :param num: Number of the current fold
+    :return: None
+    """
+    for metric in additional_metrics:
+        additional_metric_losses[metric.__name__].append(metric(y_test, train_data['oof_predictions']))
+    oof_predictions.append(train_data['oof_predictions'])
+    cv_train_losses.append(train_data['train_loss'])
+    cv_test_losses.append(train_data['test_loss'])
+    cv_logger.info(f'Fold {num} Train Loss: {train_data["train_loss"]}, Test Loss: {train_data["test_loss"]}')
+
+
+def save_cv_results_and_settings(
+    train_cfg: TrainCFG, save_directory_path: str, oof_predictions: list[Any],
+    cv_train_losses: list[Any], cv_test_losses: list[Any], additional_metric_losses: dict[str, Any]
+) -> None:
+    """Save the cross-validation results and training configuration.
+
+    :param train_cfg: Training configuration
+    :param save_directory_path: Path to run directory
+    :param oof_predictions: List containing all oof predictions of the current run
+    :param cv_train_losses: List containing all training losses of the current run
+    :param cv_test_losses: List containing all test losses of the current run
+    :param additional_metric_losses: Dict containing all lists of additional losses of the current run
+    :return: None (saves everything inside the save directory)
+    """
+    train_cfg.save(os.path.join(save_directory_path, 'train_cfg.txt'))
+    df_cv = pd.DataFrame({'train_loss': cv_train_losses, 'test_loss': cv_test_losses, **additional_metric_losses})
+    df_cv.to_csv(os.path.join(save_directory_path, 'losses.csv'), index=False)
+    np.save(os.path.join(save_directory_path, 'all_oof_predictions.npy'), np.concatenate(oof_predictions))
+
+
+def execute_cv_loop(
+    _data: pd.DataFrame, train_function: Callable, train_cfg: TrainCFG, data_cfg: DataCFG,
+    additional_metrics: Iterable[Callable] = (), **train_kwargs
+) -> None:
+    """Execute the cross-validation loop.
+
+    :param _data: Pandas dataframe containing training and testing data.
+    :param train_function: Function used to train the models.
+    :param train_cfg: Configuration used to train the models.
+    :param data_cfg: Data configuration used to train the models.
+    :param additional_metrics: Additional metrics used to evaluate the models.
+    :param train_kwargs: Additional arguments used by the train function.
+    :return: None
+    """
     save_directory_path = create_new_save_directory(train_cfg)
     time_series = get_sorted_timeseries_array(_data[data_cfg.time_column])
     splitter = TimeSeriesSplit(test_size=train_cfg.validation_time_steps, n_splits=train_cfg.num_folds)
+    cv_logger = get_logger(f'CV_{train_cfg.run_name}', train_cfg.log_dir)
 
-    oof_predictions = []
-    for train_times, test_times in splitter.split(time_series):
-        X_train = _data.loc[_data[data_cfg.time_column] <= time_series[train_times[-1]]]
-        X_test = _data.loc[time_series[test_times[-1]] <= _data[data_cfg.time_column] <= time_series[test_times[-1]]]
-        y_train, y_test = X_train.pop(data_cfg.target_column), X_test.pop(data_cfg.target_column)
+    oof_predictions, cv_train_losses, cv_test_losses = [], [], []
+    additional_metric_losses = {metric.__name__: [] for metric in additional_metrics}
+
+    for num, (train_times, test_times) in enumerate(splitter.split(time_series), start=1):
+        X_train, y_train, X_test, y_test = get_fold_data(_data, data_cfg, time_series, train_times, test_times)
         train_data = train_function(X_train, y_train, X_test, y_test, train_cfg, **train_kwargs)
-        oof_predictions.append(train_data['oof_predictions'])
-    all_oof_predictions = np.concatenate(oof_predictions)
-    np.save(save_directory_path + '/all_oof_predictions.npy', all_oof_predictions)
+        log_and_save_results(train_data, additional_metrics, additional_metric_losses,
+                             oof_predictions, cv_train_losses, cv_test_losses, cv_logger, y_test, num)
+
+    save_cv_results_and_settings(train_cfg, save_directory_path, oof_predictions, cv_train_losses, cv_test_losses,
+                                 additional_metric_losses)
