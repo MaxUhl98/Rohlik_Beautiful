94519defa4e05d93aa6a3b788bbe88f755ec072b
MaxUhl98 <frozen2ice@web.de>
2024-07-20 04:21:13 +0200

    refactored time series test script

--- unittests/test_crossvalidation/test_time_series_cv.py
+++ unittests/test_crossvalidation/test_time_series_cv.py
@@ -1,8 +1,11 @@
 import pytest
 from cross_validation.time_series_cv import *
 import pandas as pd
+
+
 def test_get_sorted_timeseries_array():
     date_series = pd.Series(['2024-01-01', '2023-01-01', '2024-01-02', '2023-01-02', '2024-02-03', '2024-02-05'])
     to_test = get_sorted_timeseries_array(date_series)
-    sorted_array = np.array(['2023-01-01', '2023-01-02','2024-01-01',  '2024-01-02', '2024-02-03', '2024-02-05'])
-    assert (to_test==sorted_array).all()
+    sorted_array = np.array(['2023-01-01', '2023-01-02', '2024-01-01', '2024-01-02', '2024-02-03', '2024-02-05'])
+    assert (to_test == sorted_array).all()
+
