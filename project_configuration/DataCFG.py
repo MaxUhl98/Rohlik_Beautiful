368e67de4ff1cb5134e13f0e470ff95f3be4b1e9
MaxUhl98 <frozen2ice@web.de>
2024-07-21 17:03:36 +0200

    added column name lists for data configuration

--- project_configuration/DataCFG.py
+++ project_configuration/DataCFG.py
@@ -2,3 +2,12 @@ class DataCFG:
     """Class that holds information about the projects data"""
     time_column: str = 'date'
     target_column = 'orders'
+
+    usable_columns: list[str] = ['warehouse', 'date', 'holiday_name', 'holiday', 'shops_closed',
+                                 'winter_school_holidays', 'school_holidays']
+
+    ordinal_columns: list[str] = ['day_of_week', 'year']
+    categorical_columns: list[str] = ['month_name', 'warehouse']
+
+    target_encoding_cols: list[str] = ['holiday_name']
+    standardize_columns: list[str] = []
