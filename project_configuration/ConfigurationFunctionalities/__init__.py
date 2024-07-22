9b56984674fef3beb394804938711c351e06eea2
MaxUhl98 <frozen2ice@web.de>
2024-07-22 07:09:05 +0200

    added key imports to package initialization files

---
+++ project_configuration/ConfigurationFunctionalities/__init__.py
@@ -0,0 +1,1 @@
+from BasicFunctionalities import BasicFunctionalities
---
+++ project_configuration/__init__.py
@@ -0,0 +1,4 @@
+from BeautyCFG import BeautyCFG
+from DataCFG import DataCFG
+from PreprocessingCFG import PreprocessingCFG
+from TrainCFG import TrainCFG
